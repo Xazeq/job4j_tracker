385a4257b29810bdf56e14b59ec868d90bbc9002
Xazeq <setezey@gmail.com>
2021-01-27 19:38:50 +0300

    6. Метод замены заявки. Tracker.replace [#433493]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -40,4 +40,30 @@ public class TrackerTest {
         assertThat("task2", is(items[0].getName()));
         assertThat("task2", is(items[1].getName()));
     }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(id, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug with description"));
+    }
+
+    @Test
+    public void whenNotReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bugWithDesc = new Item();
+        bugWithDesc.setName("Bug with description");
+        tracker.replace(5, bugWithDesc);
+        assertThat(tracker.findById(id).getName(), is("Bug"));
+    }
 }
