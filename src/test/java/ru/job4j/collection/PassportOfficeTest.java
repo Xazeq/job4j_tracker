64034069c8bf1260db07179f6c4094abaf4d57c9
Xazeq <setezey@gmail.com>
2021-03-24 16:22:35 +0300

    1. Паспорт и жители [#433406]

--- src/test/java/ru/job4j/collection/PassportOfficeTest.java
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -13,4 +13,13 @@ public class PassportOfficeTest {
         office.add(citizen);
         assertThat(office.get(citizen.getPassport()), is(citizen));
     }
+
+    @Test
+    public void whenAddSameCitizen() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        boolean result = office.add(citizen);
+        assertThat(result, is(false));
+    }
 }
