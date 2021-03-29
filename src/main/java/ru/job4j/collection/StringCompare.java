f8712a188e5b9cbf1e9040e9fd21bedfbbcb7b99
Xazeq <setezey@gmail.com>
2021-03-29 10:14:32 +0300

    3. Компаратор для строк [#433391]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+public class StringCompare {
+    public int compare(String left, String right) {
+        int minLength = Math.min(left.length(), right.length());
+        for (int i = 0; i < minLength; i++) {
+            if (left.charAt(i) != right.charAt(i)) {
+                return Character.compare(left.charAt(i), right.charAt(i));
+            }
+        }
+        return left.length() - right.length();
+    }
+}
