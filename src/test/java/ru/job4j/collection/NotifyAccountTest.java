8f3a8c544d2717ef26a93585e3d87c8a4138d181
Xazeq <setezey@gmail.com>
2021-04-20 16:24:37 +0300

    1. Метод .of [#433241]

--- src/test/java/ru/job4j/collection/NotifyAccountTest.java
+++ src/test/java/ru/job4j/collection/NotifyAccountTest.java
@@ -2,9 +2,10 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
@@ -12,12 +13,12 @@ public class NotifyAccountTest {
 
     @Test
     public void whenWithoutDuplicates() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("123", "Petr Arsentev", "eDer3432f"),
                 new Account("142", "Petr Arsentev", "000001")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                Set.of(
                         new Account("123", "Petr Arsentev", "eDer3432f"),
                         new Account("142", "Petr Arsentev", "000001")
                 )
@@ -27,13 +28,13 @@ public class NotifyAccountTest {
 
     @Test
     public void whenWithDuplicates() {
-        List<Account> accounts = Arrays.asList(
+        List<Account> accounts = List.of(
                 new Account("1234567890", "Ivan Ivanov", "54334"),
                 new Account("1020304050", "Petr Petrov", "50001"),
                 new Account("1234567890", "Ivan Ivanov", "54334")
         );
         HashSet<Account> expect = new HashSet<>(
-                Arrays.asList(
+                Set.of(
                         new Account("1234567890", "Ivan Ivanov", "54334"),
                         new Account("1020304050", "Petr Petrov", "50001")
                 )
