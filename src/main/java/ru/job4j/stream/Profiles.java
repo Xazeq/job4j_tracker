e83440cd6c69029afb9b38775675a16cf772c50c
Xazeq <setezey@gmail.com>
2021-04-20 08:51:01 +0300

    3. Уникальность элементов и сортировка. [#433232]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -7,6 +7,9 @@ public class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
+                .sorted((firstAddress, secondAddress) ->
+                        firstAddress.getCity().compareTo(secondAddress.getCity()))
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/stream/ProfilesTest.java
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.*;
 public class ProfilesTest {
 
     @Test
-    public void collect() {
+    public void whenCorrectOrderAndNoDuplicates() {
         List<Profile> profiles = List.of(
                 new Profile(new Address("City1", "Street1", 5, 12)),
                 new Profile(new Address("City2", "Street2", 2, 16)),
@@ -21,6 +21,67 @@ public class ProfilesTest {
                 new Address("City1", "Street1", 5, 12),
                 new Address("City2", "Street2", 2, 16),
                 new Address("City3", "Street3", 8, 3)
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenWrongOrderAndNoDuplicates() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("City3", "Street3", 8, 3)),
+                new Profile(new Address("City2", "Street2", 5, 12)),
+                new Profile(new Address("City4", "Street4", 2, 16)),
+                new Profile(new Address("City1", "Street1", 12, 6))
+        );
+        Profiles pr = new Profiles();
+        List<Address> rsl = pr.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("City1", "Street1", 12, 6),
+                new Address("City2", "Street2", 5, 12),
+                new Address("City3", "Street3", 8, 3),
+                new Address("City4", "Street4", 2, 16)
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenCorrectOrderAndDuplicates() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("City1", "Street1", 5, 12)),
+                new Profile(new Address("City2", "Street2", 2, 16)),
+                new Profile(new Address("City3", "Street3", 8, 3)),
+                new Profile(new Address("City2", "Street2", 2, 16)),
+                new Profile(new Address("City1", "Street1", 5, 12))
+        );
+        Profiles pr = new Profiles();
+        List<Address> rsl = pr.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("City1", "Street1", 5, 12),
+                new Address("City2", "Street2", 2, 16),
+                new Address("City3", "Street3", 8, 3)
+        );
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenWrongOrderAndDuplicates() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("City3", "Street3", 8, 3)),
+                new Profile(new Address("City2", "Street2", 5, 12)),
+                new Profile(new Address("City4", "Street4", 2, 16)),
+                new Profile(new Address("City1", "Street1", 12, 6)),
+                new Profile(new Address("City2", "Street2", 5, 12)),
+                new Profile(new Address("City1", "Street1", 12, 6)),
+                new Profile(new Address("City2", "Street2", 5, 12)),
+                new Profile(new Address("City3", "Street3", 8, 3))
+        );
+        Profiles pr = new Profiles();
+        List<Address> rsl = pr.collect(profiles);
+        List<Address> expected = List.of(
+                new Address("City1", "Street1", 12, 6),
+                new Address("City2", "Street2", 5, 12),
+                new Address("City3", "Street3", 8, 3),
+                new Address("City4", "Street4", 2, 16)
         );
         assertThat(rsl, is(expected));
     }
